738dd19e7ed4d10cb5e6a79a708d85a750012b00
mesbahs <sepideh.mesbah@tum.de>
2016-12-01 13:50:41 +0100

    narrow down the dictionary

--- playground/dictionary.py
+++ playground/dictionary.py
@@ -1,48 +1,25 @@
 
+objective = ['this research', 'this report', 'this paper', 'this study', 'this article', 'aim study', 'aim paper',
+             'aim article','purpose paper', 'purpose article', 'purpose study', 'the goal', 'we describe', 'this work',
+             'aims','we propose', 'the present paper', 'the present study', 'we study', 'we aim', 'we investigate',
+              'we discuss']
 
-"""""
-    A list of all possible verb tenses
-            ['past', '3rd singular present', 'past participle', 'infinitive',
-            'present participle', '1st singular present', '1st singular past',
-            #'past plural', '2nd singular present', '2nd singular past',
-            '3rd singular past', 'present plural']
-"""""
+method = ['we present', 'we presented', 'we develop', 'we developed' 'we conducted', 'we conduct', 'we explore',
+          'we explored', 'we construct', 'we constructed', 'we define', 'we proposed', 'we propose', 'we introduce',
+          'we introduced', 'we use', 'we used', 'algorithm', 'we analyzed', 'we analyze', 'analyzes', 'we analyse',
+          'we analysed', 'approach', 'approaches', 'methodology', 'methodologies', 'method', 'technique',
+          'we employed', 'we employ', 'we utilize', 'we utilized']
 
-objective = ['this research', 'This research', 'the aim study', 'our research', 'Our research', 'we discuss',
-             'We discuss', 'this study', 'This study', 'in this study', 'In this study', 'The goal', 'the goal',
-             'we describe', 'We describe', 'this work', 'This work', 'aims', 'we perform', 'We perform',
-             'In this paper', 'In this report', 'in this report', 'we propose', 'We propose', 'the present paper',
-             'The present paper', 'the present study', 'we study', 'We study', 'the aim of this paper',
-             'The aim of this paper', 'we aim', 'We aim', 'the purpose of this paper', 'The purpose of this paper',
-             'we investigate', 'We investigate', 'analyzed', 'analyzes', 'analyse', 'analysed',
-             'The aim of''we introduce', 'We introduce', 'in this work', 'In this work', 'we examine', 'We examine',
-             'This paper', 'this paper', 'this article', 'This article', 'we analyze', ]
+software = [' api ', 'tool', 'tools', 'obtained using', 'collected using', 'extracted using',
+            'collect using', 'obtain using', 'extract using', 'we implement', 'we implemented']
 
-dataset = ['dataset', 'Dataset', 'datasource', 'Datasource', 'data source', 'Data source', 'data from', 'Data from',
-           'collected from', 'data set', 'open data', 'OpenData', 'database', 'databases', 'Open data',
-           'geo-tagged', 'Census data', 'census data', 'mobility data', 'Mobility data', 'we collected', 'We collected',
-           'we have collected', 'We have collected', 'we extracted', 'We extracted', 'we downloaded', 'We downloaded',
-           'dataset sourced from', 'We retrieve data', 'gathered by querying', 'extracted from', 'obtained from']
+dataset = ['dataset', 'datasource', 'data source', 'collected from', 'data set', 'database', 'collect data',
+           'collected data', 'extracted data', 'extract data', 'we collected', 'we collect', 'we extract',
+           'we extracted', 'we downloaded', 'dataset sourced from', 'retrieve data', 'retrieved data', 'extracted from',
+           'obtained from']
 
+result = ['we find', 'precision', 'recall', 'f1', 'shows', 'show', 'shown', 'showed', 'we found', 'was found',
+          'were found','findings', 'result', 'Figure', 'figure', 'table', 'Table', 'we observe', 'we observed',
+          'we compared', 'we compare', 'we see']
 
-method = ['We present', 'we present', 'We compare', 'we compare', 'we develop',
-          'We develop', 'We conducted', 'we conducted', 'we explore', 'We explore', 'we construct', 'we define',
-          'We define', 'We construct', 'We propose', 'We proposed', 'we proposed', 'we propose', 'we introduce',
-          'We introduce','we introduced', 'We introduced', 'we use', 'We use', 'we used', 'We used', 'Algorithm', 
-          'algorithm','we employ', 'utilize', 'utilized','methodology', 'Methodology',  'We implemented',
-          'analyze', 'analyzed', 'analyzes', 'analyse', 'analysed', 'analyses', 'approach', 'approaches', 'Aproach',
-          'Methodologies', 'methodologies', 'method', 'Method', 'technique', 'Technique', 'We implement',
-          'we implement', 'implemented', 'implementing', 'We then use', 'we then use', 'we developed', 'we employed',
-          ]
-
-software = [' API ', ' api ', 'tool', 'software', 'Tool', 'Software', 'tools', 'Tools', ' Api ', 'obtained using',
-            'data were collected using', 'performed by', 'performed using', 'extracted using']
-
-result = ['we can observe', 'We can observe', 'we find', 'we can see', 'precision',
-          'recall', 'F1', 'F 1', 'shows', 'show', 'shown', 'showed', 'we found', 'was found', 'were found', 'findings',
-          'result', 'Figure', 'figure', 'table', 'Table', 'we observe', 'we observed', 'We plot', 'we plot',
-          'we have measured', 'Our method predicts', 'We have measured',
-          'we generated', 'we computed', 'We computed', 'We have computed', 'we measured', 'We measured', 'We measure',
-          'we measure', 'we compared', 'We compared'
-          ]
 
