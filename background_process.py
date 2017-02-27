010d63625f375e849040c5a5c26b8bc5abe96bbe
KyriakosFrang <kyriakos.frangeskos@gmail.com>
2017-02-27 19:56:11 +0100

    Create class for text extraction (needs testing)

--- background_process.py
+++ background_process.py
@@ -1,8 +1,10 @@
 from dblp_xml_processing import XmlProcessing
+from pdf_text_extractor import TextExtraction
 
 
 def main():
   xml_process = XmlProcessing()
+  txt_extraction = TextExtraction()
 
 
 if __name__ == '__main__':
