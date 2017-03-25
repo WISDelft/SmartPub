1a55c31ffb0643e423be412eeac59390c5bc2eda
KyriakosFrang <kyriakos.frangeskos@gmail.com>
2017-03-25 17:00:16 +0100

    Clusters

--- method_co_occur_DBSCAN.py
+++ method_co_occur_DBSCAN.py
@@ -156,11 +156,13 @@ def main():
   print("Fit SVD+Normalization")
   X = lsa.fit_transform(Xc)
 
+  with open(cfg.folder_pickle + 'X_dbscan_multilabel_DataPipelines_{}.pkl'.format(1), 'wb') as vec:
+    pkl.dump(X, vec)
 
   explained_variance = svd.explained_variance_ratio_.sum()
   print("Explained variance of the SVD step: {}%".format(int(explained_variance * 100)))
 
-  db = DBSCAN(eps=0.3, min_samples=20).fit(X)
+  db = DBSCAN(eps=0.1, min_samples=40).fit(X)
   core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
   core_samples_mask[db.core_sample_indices_] = True
   labels = db.labels_
