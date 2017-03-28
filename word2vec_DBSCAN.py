9655b4eb12e7feeff2094d3293d897506ffbe060
KyriakosFrang <kyriakos.frangeskos@gmail.com>
2017-03-28 15:32:53 +0200

    Clusters

--- word2vec_DBSCAN.py
+++ word2vec_DBSCAN.py
@@ -30,7 +30,7 @@ def main():
     # w2v_array = normalizer.transform(w2v_array)
 
     # DBSCAN
-    db = DBSCAN(eps=0.4, min_samples=20).fit(w2v_array)
+    db = DBSCAN(eps=0.3, min_samples=60).fit(w2v_array)
     core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
     core_samples_mask[db.core_sample_indices_] = True
     labels = db.labels_
